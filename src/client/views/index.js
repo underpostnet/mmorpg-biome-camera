a501e30e2faa6c067e050597f557423038f61d93
fcoverdugo <fcoverdugoa@underpost.net>
2023-10-04 15:45:58 -0300

    update

--- src/client/views/index.js
+++ src/client/views/index.js
@@ -1,8 +1,8 @@
 const IndexView = {
   path: '/',
   node_modules: ['./node_modules/pixi.js/dist/pixi.min.js', './node_modules/socket.io/client-dist/socket.io.min.js'],
-  componets: ['index', 'pixi', 'grid', 'bag', 'socketIo'],
-  services: [],
+  componets: ['index', 'pixi', 'grid', 'bag', 'socketIo', 'biome'],
+  services: ['biome'],
   libs: ['vanilla', 'css', 'colors'],
   title: 'C Y B E R I A',
 };
